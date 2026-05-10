0ddde61fc1dbae3fb7e25a750c9dc0d727e3489a
volts-money <Volts2169@gmail.com>
2026-05-10 18:38:58 -0400

    update

---
+++ package.js
@@ -0,0 +1,12 @@
+{
+  "name": "clothing-brand-bot",
+  "version": "1.0.0",
+  "main": "bot.js",
+  "scripts": {
+    "start": "node bot.js"
+  },
+  "dependencies": {
+    "discord.js": "^14.0.0",
+    "dotenv": "^16.0.0"
+  }
+}
